7a40757c23dc58aad08508863c8afee7a2e225a3
codershona <falgunimst95@gmail.com>
2020-04-08 11:38:06 +0200

    working on my project structure and setup done

---
+++ src/app_component/weather.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Weather = () => {
+	return (
+		<div className="container">
+		<h1>REACT WEATHER API</h1>
+
+		</div>
+
+
+   );
+};
+
+export default Weather;
