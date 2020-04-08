769d047ba2ce772a8517689d602a6bc330d21fea
codershona <falgunimst95@gmail.com>
2020-04-08 16:07:32 +0200

    get_Weather Icon using this method i can get Data from API of right ID Property

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
     Atmosphere: "wi-fog",
     Clear: "wi-day-sunny",
     Clouds: "wi-day-fog"
-    
+
   };
 
 }
@@ -43,6 +43,49 @@ class App extends React.Component {
     let cell = Math.floor(temp - 273.15);
     return cell;
 
+  }
+
+  get_WeatherIcon(icons, rangeID) {
+    switch(true) {
+    
+         case rangeID >= 200 && rangeID <= 232:
+      this.setState({icon: this.weatherIcon.Thunderstorm});
+      break;
+
+       
+          case rangeID >= 300 && rangeID <= 321:
+      this.setState({icon: this.weatherIcon.Drizzle});
+      break;
+
+           case rangeID >= 500 && rangeID <= 531:
+      this.setState({icon: this.weatherIcon.Rain});
+      break;
+
+
+         case rangeID >= 600 && rangeID <= 622:
+      this.setState({icon: this.weatherIcon.Snow});
+      break;
+
+
+         case rangeID >= 701 && rangeID <= 781:
+      this.setState({icon: this.weatherIcon.Atmosphere});
+      break;
+
+
+         case rangeID === 800:
+      this.setState({icon: this.weatherIcon.Clear});
+      break;
+
+
+         case rangeID >= 801 && rangeID <= 804:
+      this.setState({icon: this.weatherIcon.Clouds});
+      break;
+
+       default:
+          this.setState({icon: this.weatherIcon.Clouds}) 
+
+
+    }
   }
 
 
@@ -60,11 +103,11 @@ class App extends React.Component {
     celsius: this.calCelsius(response.main.temp),
     temp_max: this.calCelsius(response.main.temp_max),
     temp_min: this.calCelsius(response.main.temp_min),
-    description: response.weather[0].description,
-    icon: this.weatherIcon.Thunderstorm
+    description: response.weather[0].description
+    
+    });
 
-
-   });
+   this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
 
 
  };
