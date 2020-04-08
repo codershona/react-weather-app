34b318843441c7a7ca4590a902aff4081a42f45d
codershona <falgunimst95@gmail.com>
2020-04-08 18:21:22 +0200

    error message setup done

--- src/App.js
+++ src/App.js
@@ -113,11 +113,13 @@ class App extends React.Component {
 
    this.setState({
     city: `${response.name}, ${response.sys.country}`,
-    
+
     celsius: this.calCelsius(response.main.temp),
     temp_max: this.calCelsius(response.main.temp_max),
     temp_min: this.calCelsius(response.main.temp_min),
-    description: response.weather[0].description
+    description: response.weather[0].description,
+
+    error: false
     
     });
 
