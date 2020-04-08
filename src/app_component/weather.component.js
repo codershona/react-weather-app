14a8d424974abaf0704fc2b8039d28a356f77fa9
codershona <falgunimst95@gmail.com>
2020-04-08 11:40:36 +0200

    adding div containers and cards in my method concepts

--- src/app_component/weather.component.js
+++ src/app_component/weather.component.js
@@ -4,9 +4,11 @@ import React from 'react';
 const Weather = () => {
 	return (
 		<div className="container">
-		<h1>REACT WEATHER API</h1>
+		 <div className="cards">
+           <h1>New York</h1>
 
-		</div>
+		 </div>
+		 </div>
 
 
    );
